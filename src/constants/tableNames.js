c4fdae322a9f2de8d4f6805a048fa7f116753f4c
Santiago Cardona <santiagocardo80@gmail.com>
2020-05-12 22:00:04 -0500

    :tada: Initial commit

---
+++ src/constants/tableNames.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  user: 'user',
+  item_type: 'item_type',
+  state: 'state',
+  country: 'country',
+  shape: 'shape',
+  location: 'location',
+  address: 'address',
+  manufacturer: 'manufacturer'
+}
